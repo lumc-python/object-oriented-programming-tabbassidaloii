272e3bb3e6b6fceb1f8dae070cb9c3a85bbe4aa5
jkvis <jkvis@users.noreply.github.com>
2017-09-15 17:55:00 +0200

    Create fraction.py

---
+++ fraction.py
@@ -0,0 +1,11 @@
+class Fraction(object):
+    """
+    A simple fraction class.
+    """
+
+    def __init__(self, numerator, denominator=1):
+        """
+        Creates a new fraction.
+        """
+        self._numerator = numerator
+        self._denominator = denominator
